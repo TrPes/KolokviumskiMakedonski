f5eee2e983556d318731644c0c66c13f8aa75dcf
Trpeskiii <126724325+TrPes@users.noreply.github.com>
2024-11-15 12:06:54 +0100

    Update osma.cpp

--- osma.cpp
+++ osma.cpp
@@ -4,24 +4,28 @@ using namespace std;
 int main() {
     int n,x;
     cin>>n>>x;
-    int cifraN=0,cifraX=0, maxRazl = 0,temp,tempX;
+    int cifraN=0,cifraX=0, maxRazl = 0;
     for(int i=n-1;i>=0;i--)
     {
-        temp=i;
-        tempX=x;
-        while(temp!=0) {
+        int temp=i;
+        int tempX=x;
+        while(temp>0) {
             cifraN = temp % 10;
             cifraX = tempX % 10;
-            if (cifraX == cifraN){break;}
-            else{
-                tempX /= 10;
-                if(tempX==0){
-                    tempX=x;
-                    temp/=10;
-                }
+            if (cifraX == cifraN){
+                break;
+            }
+            
+         tempX /= 10;
+         if(tempX==0){
+          tempX=x;
+         temp/=10;  
             }
         }
-        if(temp==0){maxRazl=i;break;}
+        if(temp==0){
+            maxRazl=i;
+            break;
+        }
     }
     cout<<maxRazl;
     return 0;
